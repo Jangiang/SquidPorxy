be0960dfc0d1a781fc3404875e81b94c07884ba3
Jangiang <142575248+Jangiang@users.noreply.github.com>
2023-09-10 16:25:28 +0700

    Update and rename squid-install.sh to giang.sh

--- giang.sh
+++ giang.sh
@@ -6,4 +6,4 @@ sudo service squid start #start Squid
 sudo chkconfig squid on #start Squid on boot
 touch /etc/squid/passwd #create a file that will later store the username for the authentication.
 chown squid: /etc/squid/passwd #set owner for password file
-htpasswd /etc/squid/passwd proxyclient #create a user and set password
+htpasswd /etc/squid/passwd giang #create a user and set password
