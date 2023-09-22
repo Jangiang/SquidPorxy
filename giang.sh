38251fb2ad50b7045958f3a20ed90933d0d2a496
Jangiang <142575248+Jangiang@users.noreply.github.com>
2023-09-22 17:24:13 +0700

    Delete giang.sh

--- giang.sh
+++
@@ -1,9 +0,0 @@
-sudo yum update #update OS
-sudo yum upgrade #upgrade OS
-sudo yum install -y squid #install Squid
-sudo yum -y install httpd-tools #install httpd-tools
-sudo service squid start #start Squid
-sudo chkconfig squid on #start Squid on boot
-touch /etc/squid/passwd #create a file that will later store the username for the authentication.
-chown squid: /etc/squid/passwd #set owner for password file
-htpasswd /etc/squid/passwd giang #create a user and set password
