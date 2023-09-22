0febf6d2bd1ef21fdf45fa70ff3f50deb89c317e
Jangiang <142575248+Jangiang@users.noreply.github.com>
2023-09-22 17:23:33 +0700

    Update rs100.sh

--- rs100.sh
+++ rs100.sh
@@ -186,7 +186,7 @@ IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
 echo "How many proxy do you want to create? Example 500"
-COUNT=20  # Set the number of proxies you want
+COUNT=100  # Set the number of proxies you want
 echo "$COUNT"
 #read COUNT
 
