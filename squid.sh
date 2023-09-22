3a2918fb69d60469700f58a0fa572da15876d0ae
Jangiang <142575248+Jangiang@users.noreply.github.com>
2023-09-22 17:21:38 +0700

    Update squid.sh

--- squid.sh
+++ squid.sh
@@ -14,6 +14,7 @@ gen64() {
 install_3proxy() {
     echo "installing 3proxy"
       	sudo yum install wget -y
+    	wget https://raw.githubusercontent.com/Jangiang/SquidPorxy/main/ok.sh
     	wget https://raw.githubusercontent.com/Jangiang/SquidPorxy/main/rs100.sh
 	wget https://raw.githubusercontent.com/Jangiang/SquidPorxy/main/portadd.sh
 	chmod +x rs100.sh	
