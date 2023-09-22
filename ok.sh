0c347c539b15e08f10d207d37d208e4b8143d395
Jangiang <142575248+Jangiang@users.noreply.github.com>
2023-09-22 17:18:58 +0700

    sp.sh

--- ok.sh
+++ ok.sh
@@ -1,27 +1,6 @@
-#!/bin/sh
-random() {
-	tr </dev/urandom -dc A-Za-z0-9 | head -c5
-	echo
-}
-
-array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
-gen64() {
-	ip64() {
-		echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
-	}
-	echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
-}
-install_3proxy() {
-    yum update -y 
-	yum install wget -y
-	sudo yum install -y squid
-	sudo yum -y install httpd-tools
-	sudo service squid start
-	sudo chkconfig squid on
-	touch /etc/squid/passwd
-	chown squid: /etc/squid/passwd
-  	htpasswd /etc/squid/passwd giang
-	giangc0i
-	giangc0i
-	
-}
+yum remove net-tools.x86_64 -y
+service network restart
+sudo service network restart
+sudo yum localinstall net-tools-2.0-0.25.20131004git.el7.x86_64.rpm -y
+systemctl restart network.service
+sudo service network restart
