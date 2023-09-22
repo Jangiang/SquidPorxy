97ccc67bad932d0a4e16b867ed47f7347763f60f
Jangiang <142575248+Jangiang@users.noreply.github.com>
2023-09-22 23:49:37 +0700

    Update sp.sh

--- sp.sh
+++ sp.sh
@@ -1,4 +1,5 @@
 yum remove net-tools.x86_64 -y
+yum install network-scripts
 service network restart
 wget https://github.com/Jangiang/SquidPorxy/raw/main/net-tools-2.0-0.25.20131004git.el7.x86_64.rpm
 sudo yum localinstall net-tools-2.0-0.25.20131004git.el7.x86_64.rpm -y
