1f5fd2e7f2872197efe1f2a7ea403a51b6416ba7
Jangiang <142575248+Jangiang@users.noreply.github.com>
2023-10-25 14:56:31 +0700

    Update squid.sh

--- squid.sh
+++ squid.sh
@@ -183,7 +183,7 @@ EOF
 
 gen_ifconfig() {
     cat <<EOF
-$(awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' ${WORKDATA})
+$(awk -F "/" '{print "ifconfig ens192 inet6 add " $5 "/64"}' ${WORKDATA})
 EOF
 }
 echo "installing apps"
